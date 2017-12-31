70d6b42fabccb4a9fc4830bbb4ba3615a68bfce3
Scoowy <gahonajuanjo@gmail.com>
2017-12-30 21:20:01 -0500

    Mas entradas
    Añadido:
    Nuevas clases con sus respectivos repositorios
    -Entradas de imagenes
    -Entradas de codigos de programas

---
+++ app/EntradaProg.inc.php
@@ -0,0 +1,73 @@
+<?php
+
+class Entrada {
+    
+    private $id;
+    private $autorId;
+    private $lenguajeId;
+    private $titulo;
+    private $texto;
+    private $imagen = 'img/imgPrueba.jpg';
+    private $fecha;
+    private $activa;
+    
+    public function __construct($id, $autorId, $lenguajeId, $titulo, $texto, $imagen, $fecha, $activa) {
+        $this -> id = $id;
+        $this -> autorId = $autorId;
+        $this -> lenguajeId = $lenguajeId;
+        $this -> titulo = $titulo;
+        $this -> texto = $texto;
+        $this -> imagen = $imagen;
+        $this -> fecha = $fecha;
+        $this -> activa = $activa;
+    }
+
+    public function obtenerId() {
+        return $this -> id;
+    }
+
+    public function obtenerAutorId() {
+        return $this -> autorId;
+    }
+    
+    public function obtenerLenguajeId() {
+        return $this -> lenguajeId;
+    }
+
+    public function obtenerTitulo() {
+        return $this -> titulo;
+    }
+
+    public function obtenerTexto() {
+        return $this -> texto;
+    }
+    
+    public function obtenerImagen() {
+        return $this -> imagen;
+    }
+
+    public function obtenerFecha() {
+        return $this -> fecha;
+    }
+
+    public function obtenerActiva() {
+        return $this -> activa;
+    }
+
+    public function cambiarTitulo($titulo) {
+        $this -> titulo = $titulo;
+    }
+
+    public function cambiarTexto($texto) {
+        $this -> texto = $texto;
+    }
+    
+    public function cambiarImagen($imagen) {
+        $this -> imagen = $imagen;
+    }
+
+    public function cambiarActiva($activa) {
+        $this -> activa = $activa;
+    }
+
+}
---
+++ app/RepositorioEntradaImg.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once 'app/config.inc.php';
+include_once 'app/Conexion.inc.php';
+include_once 'app/EntradaImg.inc.php';
+
+class RepositorioEntradaImg {
+    
+    public static function insertarEntradaImg($conexion, $entradaImg) {
+        $entradaImgInsertada = false;
+        
+        if (isset($conexion)) {
+            try {
+                $sql = "INSERT INTO entradasImg(autor_id, titulo, texto, imagen, fecha, ubicacion, activa) VALUES(:autorId, :titulo, :texto, :imagen , NOW(), :ubicacion, 0)";
+
+                $autorIdTemp = $entradaImg -> obtenerAutorId();
+                $tituloTemp = $entradaImg -> obtenerTitulo();
+                $textoTemp = $entradaImg -> obtenerTexto();
+                $imagenTemp = $entradaImg -> obtenerImagen();
+                $ubicacionTemp = $entradaImg -> obtenerUbicacion();
+
+                $sentencia = $conexion -> prepare($sql);
+
+                $sentencia -> bindParam(':autorId', $autorIdTemp, PDO::PARAM_STR);
+                $sentencia -> bindParam(':titulo', $tituloTemp, PDO::PARAM_STR);
+                $sentencia -> bindParam(':texto', $textoTemp, PDO::PARAM_STR);
+                $sentencia -> bindParam(':imagen', $imagenTemp, PDO::PARAM_STR);
+                $sentencia -> bindParam(':ubicacion', $ubicacionTemp, PDO::PARAM_STR);
+                
+                $entradaImgInsertada = $sentencia -> execute();
+                
+            } catch (PDOException $ex) {
+                print 'ERROR' . $ex->getMessage();
+            }
+        }
+        
+        return $entradaImgInsertada;
+    }
+}
